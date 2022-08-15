caf09543f5fc910db8b661f6c3d83a877d65c6ac
Gulyutin <gulyutin@gmail.com>
2022-08-15 15:17:46 +0700

    EX47 added

--- EX47/Program.cs
+++ EX47/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+Random rand = new Random();
+for (int i = 0; i < m; i++)
+{
+    for (int  j = 0; j < n; j++)
+    {
+        array[i, j] = rand.Next();
+        Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine("");
+}
